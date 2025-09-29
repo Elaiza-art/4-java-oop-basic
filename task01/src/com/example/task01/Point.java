ce44d0519fe7ceadd67fb13018d5ea9b70e66e11
Elaiza-art <liza.kupidonli25@mail.ru>
2025-09-29 22:37:57 +0500

    Галичина Елизавета. Задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,32 @@ public class Point {
     int x;
     int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public  Point(){
+        this(0,0);
+    }
+
+    void flip(){
+        int item = x;
+        x = - y;
+        y = - item;
+    }
+
+    double distance(Point point){
+        double distanceX = point.x - this.x;
+        double distanceY = point.y - this.y;
+        double result = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
+        return result;
+    }
+
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
