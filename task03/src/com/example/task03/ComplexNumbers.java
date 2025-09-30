c2adcf7660276e8a1b735faa64898889b397b503
Elaiza-art <liza.kupidonli25@mail.ru>
2025-09-30 13:50:44 +0500

    Галичина Елизавета. Задание 3

---
+++ task03/src/com/example/task03/ComplexNumbers.java
@@ -0,0 +1,41 @@
+package com.example.task03;
+
+public class ComplexNumbers {
+
+    private final double realPart;
+    private final double imaginaryPart;
+
+    public ComplexNumbers( double realPart, double imaginaryPart){
+        this.realPart = realPart;
+        this.imaginaryPart = imaginaryPart;
+    }
+
+    public ComplexNumbers add(ComplexNumbers comples){
+        return new ComplexNumbers(
+                this.realPart + comples.realPart,
+                this.imaginaryPart +  comples.imaginaryPart);
+    }
+
+    public ComplexNumbers product(ComplexNumbers complex){
+        double newReal = (this.realPart * complex.realPart) - (this.imaginaryPart * complex.imaginaryPart);
+        double newImaginary = this.realPart * complex.imaginaryPart + this.imaginaryPart * complex.realPart;
+        return new ComplexNumbers(newReal, newImaginary);
+    }
+
+    public String toString() {
+        if (realPart == 0) {
+            // Только мнимая часть
+            if (imaginaryPart == 0) {
+                return String.format("Комплексное число: 0");
+            } else {
+                return String.format("Комплексное число: %.2fi", imaginaryPart);
+            }
+        } else if (imaginaryPart > 0) {
+            return String.format("Комплексное число: %.2f + %.2fi", realPart, imaginaryPart);
+        } else if (imaginaryPart == 0) {
+            return String.format("Комплексное число: %.2f", realPart);
+        } else {
+            return String.format("Комплексное число: %.2f - %.2fi", realPart, Math.abs(imaginaryPart));
+        }
+    }
+}
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumbers complex1 = new ComplexNumbers(0,2);
+        ComplexNumbers complex2 = new ComplexNumbers(1,-4);
 
+        System.out.println(complex1);
+        System.out.println(complex2);
+
+        System.out.println(complex1.add(complex2));
+        System.out.println(complex1.product(complex2));
     }
 }
