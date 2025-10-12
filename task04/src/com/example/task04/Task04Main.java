58af0abe7f61bd6c75320b63f464b06802793484
Elaiza-art <liza.kupidonli25@mail.ru>
2025-10-12 23:44:53 +0500

    Галичина Елизавета. Задание 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -3,5 +3,20 @@ package com.example.task04;
 public class Task04Main {
     public static void main(String[] args) {
 
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(4,5);
+        Line section = new Line(p1,p2);
+
+        Point startingPoint = section.getP1();
+        Point enddingPoint = section.getP2();
+
+        System.out.println("Начало отрезка - " + startingPoint);
+        System.out.println("Конец отрезка - " + enddingPoint);
+
+        System.out.println(section);
+
+        Point p3 = new Point(2,3);
+        System.out.println("Точка" + p3 + " на прямой: " + section.isCollinearLine(p3));
+
     }
 }
