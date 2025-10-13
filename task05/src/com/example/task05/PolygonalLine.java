b127267a907c5bff890aa3792c8e15144a0770a0
Elaiza-art <liza.kupidonli25@mail.ru>
2025-10-13 17:54:18 +0500

    Галичина Елизавета. Задание 5

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,9 +1,32 @@
 package com.example.task05;
 
+import sun.security.util.Length;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+
+    private Point[] points;
+
+    public PolygonalLine() {
+        this.points = new Point[0];  // Создаем пустой массив точек
+    }
+
+    public PolygonalLine(Point[] points) {
+        if (points == null) {
+            this.points = new Point[0];
+        } else {
+            this.points = new Point[points.length];
+
+            for (int i = 0; i < points.length; i++){
+                if(points[i] != null){
+                    this.points[i] = new Point(points[i].getX(), points[i].getY());
+                }
+            }
+        }
+    }
 
     /**
      * Устанавливает точки ломаной линии
@@ -12,6 +35,16 @@ public class PolygonalLine {
      */
     public void setPoints(Point[] points) {
         // TODO: реализовать
+        if (points == null) {
+            this.points = new Point[0];
+        } else {
+            this.points = new Point[points.length];
+            for (int i = 0; i < points.length; i++) {
+                if (points[i] != null) {
+                    this.points[i] = new Point(points[i].getX(), points[i].getY());
+                }
+                }
+        }
     }
 
     /**
@@ -21,6 +54,17 @@ public class PolygonalLine {
      */
     public void addPoint(Point point) {
         // TODO: реализовать
+        if (point == null) return;
+
+        Point newPoin = new Point(point.getX(), point.getY());
+
+        Point[] newPoint = new Point[points.length+1];
+
+        for (int i = 0; i < points.length; i++){
+            newPoint[i] = points[i];
+        }
+        newPoint[points.length] = newPoin;
+        points = newPoint;
     }
 
     /**
@@ -31,6 +75,8 @@ public class PolygonalLine {
      */
     public void addPoint(double x, double y) {
         // TODO: реализовать
+        Point point = new Point(x, y);
+        addPoint(point);
     }
 
     /**
@@ -40,7 +86,14 @@ public class PolygonalLine {
      */
     public double getLength() {
         // TODO: реализовать
-        throw new AssertionError();
+        if (points.length < 2) return 0.0;
+
+        double lineLength = 0.0;
+
+        for (int i = 0; i < points.length - 1; i++){
+            lineLength += points[i].getLength(points[i + 1]);
+        }
+        return lineLength;
     }
 
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -3,5 +3,15 @@ package com.example.task05;
 public class Task05Main {
     public static void main(String[] args) {
 
+        PolygonalLine line = new PolygonalLine();
+        line.setPoints(new Point[]{
+                new Point(2,3),
+                new Point(4,4),
+                new Point(1,3),
+                new Point(7,2)
+        });
+        line.addPoint(0,5);
+        System.out.println("Длина ломаной: " + line.getLength());
+
     }
 }
