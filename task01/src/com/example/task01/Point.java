adac4113268adc7220aacf9785e8071ea5dba569
Elaiza-art <liza.kupidonli25@mail.ru>
2025-11-17 21:53:50 +0500

    Галичина Елизавета. Изменения в 1 задание

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -34,7 +34,6 @@ public class Point {
     }
 
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(toString());
     }
 }
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,18 +2,22 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.x = 10;
-        p1.y = 45;
+        Point p1 = new Point(10,45);
+        p1.x = 11;
+        p1.y = 46;
         Point p2 = new Point();
         p2.x = 78;
         p2.y = 12;
 
+        p1.flip();
+        System.out.println(p1);
+
+        System.out.println(p1.distance(p2));
+
+
         System.out.println("Point 1:");
-        p1.print();
         System.out.println(p1);
         System.out.println("Point 2:");
-        p2.print();
         System.out.println(p2);
     }
 }
