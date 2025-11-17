b4851a50936ef23c01670f613a5b21c5136ad344
Elaiza-art <liza.kupidonli25@mail.ru>
2025-11-17 23:51:42 +0500

    Галичина Елизавета. Изменения в 1-2 задание

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,31 +3,13 @@ package com.example.task02;
 public class Task02Main {
     public static void main(String[] args) {
         TimeSpan time1 = new TimeSpan(1,100,130);
+        System.out.println("Время 1 до ");
+        System.out.println(time1);
         time1.setSeconds(3000);
-        System.out.println("Время 1" + time1);
-        TimeSpan time2 = new TimeSpan(0,90,0);
-        System.out.println("Время 2" + time2);
+        System.out.println("Время 1 после");
+        System.out.println(time1);
 
-        System.out.println("Часы: " + time1.getHours());
-        System.out.println("Минуты: " + time1.getMinutes());
-        System.out.println("Секунды: " + time1.getSeconds());
+        time1.setSeconds(-1);
 
-        time1.setMinutes(75);
-        System.out.println("После установки значения: " + time1);
-        System.out.println("Минуты: " + time1.getMinutes());
-
-        TimeSpan time3 = new TimeSpan(-1, 30, 0);
-        TimeSpan time4 = new TimeSpan(0, 45, 30);
-
-        time3.add(time4);
-        System.out.println("После метода .add: " + time3);
-
-        TimeSpan time5 = new TimeSpan(2, 0, 0);
-        TimeSpan time6 = new TimeSpan(0, 45, 0);
-        System.out.println("Время 5: " + time5);
-        System.out.println("Время 6: " + time6);
-
-        time5.subtract(time6);
-        System.out.println("После метода .subtract: " + time5);
     }
 }
