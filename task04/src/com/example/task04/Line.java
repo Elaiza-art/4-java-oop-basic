73e0f9bf91d9b8fb272bdf94eaf93037e0c868eb
Elaiza-art <liza.kupidonli25@mail.ru>
2025-11-18 09:08:04 +0500

    Галичина Елизавета. Изменения в 1-4 задания

--- task04/src/com/example/task04/Line.java
+++ task04/src/com/example/task04/Line.java
@@ -3,13 +3,16 @@ package com.example.task04;
 import java.awt.*;
 
 class Point{
-    public final int x;
-    public final int y;
+    private final int x;
+    private final int y;
 
     public Point(int x, int y){
         this.x = x;
         this.y = y;
     }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
 
     public String toString() {
         return "(" + x + ", " + y + ")";
@@ -38,14 +41,14 @@ public class Line {
 
     public String toString(){
         return String.format("Отрезок с началом в (%d, %d) и с концом в (%d, %d)",
-                p1.x, p1.y, p2.x, p2.y);
+                p1.getX(), p1.getY(), p2.getX(), p2.getY());
     }
 
     public boolean isCollinearLine(Point p){
 
-        int x = p.x;
-        int y = p.y;
-        int area = (x - p1.x) * (p2.y - p1.y) - (y - p1.y) * (p2.x - p1.x);
+        int x = p.getX();
+        int y = p.getY();
+        int area = (x - p1.getX()) * (p2.getY() - p1.getY()) - (y - p1.getY()) * (p2.getX() - p1.getX());
         return area == 0;
     }
 
